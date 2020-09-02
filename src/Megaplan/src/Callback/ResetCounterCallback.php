6210880ea1a3b1e06251b2bb6e228452687220cb
Yurii Orlyk <orlyk.rollun@gmail.com>
2020-09-02 12:18:03 +0300

    added reset counter callback

---
+++ src/Megaplan/src/Callback/ResetCounterCallback.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace rollun\api\megaplan\Callback;
+
+
+use Psr\Log\LoggerInterface;
+use rollun\dic\InsideConstruct;
+use rollun\logger\Writer\PrometheusWriter;
+
+class ResetCounterCallback
+{
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function __wakeup()
+    {
+        InsideConstruct::initWakeup([
+            'logger' => LoggerInterface::class,
+        ]);
+    }
+
+    public function __invoke()
+    {
+        $this->logger->notice('METRICS_COUNTER', [
+            PrometheusWriter::METRIC_ID => 'megaplan_requests',
+            PrometheusWriter::VALUE => 0,
+            PrometheusWriter::REFRESH => true,
+        ]);
+    }
+}
